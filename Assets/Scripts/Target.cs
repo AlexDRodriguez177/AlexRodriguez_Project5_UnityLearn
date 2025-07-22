2ba8251a1736bf02cc6acf8c4e285b21d338aa4e
Alex Rodriguez <alex.rodriguez6@g.austincc.edu>
2025-07-21 20:41:27 -0500

    Created methods for the different calculations (SpawnPos, Torque, Force/Speed)
    Added Summary Commnets

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -16,18 +16,39 @@ public class Target : MonoBehaviour
     /// </summary>
     void Start()
     {
-       targetRb = GetComponent<Rigidbody>();
-       
-       targetRb.AddForce(Vector3.up * Random.Range(minStrength, maxStrength), ForceMode.Impulse);
-       
-       targetRb.AddTorque(Random.Range(-torqueRange, torqueRange), Random.Range(-torqueRange, torqueRange), Random.Range(-torqueRange, torqueRange), ForceMode.Impulse);
-       
-       transform.position = new Vector3(Random.Range(-spawnXRange, spawnXRange), -spawnYPosition);
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPosition();
     }
 
-    
+
     void Update()
     {
-        
+
+    }
+    /// <summary>
+    /// Calculates a random force and multiplys it by s Vector3.up 
+    /// </summary>
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minStrength, maxStrength);
+    }
+    /// <summary>
+    /// Calculates a randome torque for the objects
+    /// </summary>
+    float RandomTorque()
+    {
+        return Random.Range(-torqueRange, torqueRange);
+    }
+    /// <summary>
+    /// Calculates a random spawn position for the target objects
+    /// </summary>
+    Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-spawnXRange, spawnXRange), -spawnYPosition);
     }
 }
